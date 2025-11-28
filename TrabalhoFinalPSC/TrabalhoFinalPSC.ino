60b54787fd4c90206b4ba0fad682e5685acf3595
JoseArantes83 <josearantes20@hotmail.com>
2025-11-28 15:50:39 -0300

    feat: add bomb 1, LCD and timer logic to arduino code

--- TrabalhoFinalPSC/TrabalhoFinalPSC.ino
+++ TrabalhoFinalPSC/TrabalhoFinalPSC.ino
@@ -1,28 +1,45 @@
+#include <LiquidCrystal.h>
 #include <SimpleModbusSlave.h>
 
-// ====== Pinos dos Ultrassônicos ======
-#define TRIG_1 6
-#define ECHO_1 7
-#define TRIG_2 8
-#define ECHO_2 9
+#define TRIG_1 A5
+#define ECHO_1 A4
+#define TRIG_2 A3
+#define ECHO_2 A2
 
-// ====== Pinos das Bombas ======
-#define BOMBA1P 4  //in1 ponte H
-#define BOMBA1N 5  //in2 ponte H
+#define BOMBA1P 5  //in1 ponte H
+#define BOMBA1N 4  //in2 ponte H
 #define BOMBA2P 3  //in3 ponte H
 #define BOMBA2N 2  //in4 ponte H
 
-// ====== Registradores Modbus ======
 enum {
   DIST_1,      // distância sensor 1 (cm)
   DIST_2,      // distância sensor 2 (cm)
+  BOMBA1_PWM,  // comando PWM da bomba 1 (0–255)
   BOMBA2_PWM,  // comando PWM da bomba 2 (0–255)
   HOLDING_REGS_SIZE
 };
 
+const int d4 = 6, d5 = 7, d6 = 8, d7 = 9, rs = 10, en = 11;
+const int backLight = 12;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 uint16_t holdingRegs[HOLDING_REGS_SIZE];
 
-// ===== Função genérica para ler ultrassônico =====
+struct {
+  int limite;
+  char *nome;
+}
+
+teclas[] = {
+  { 50, "Direita   " },
+  { 150, "Cima     " },
+  { 300, "Baixo    " },
+  { 500, "Esquerda " },
+  { 750, "Select    " },
+  { 0, "Reset       " },
+  { 1024, "            " }
+};
+
 long readUltrasonic(int trigPin, int echoPin) {
   long duration;
 
@@ -54,43 +71,69 @@ void ligaBomba2(int porcentagem) {
   analogWrite(BOMBA2P, porcentagemLiga);
 }
 
+unsigned long ultimoTempo = 0;
+const unsigned long intervalo = 100;  // 1 segundo em ms
+
 void setup() {
   modbus_configure(&Serial, 9600, SERIAL_8N1, 1, 0, HOLDING_REGS_SIZE, holdingRegs);
 
   Serial.begin(9600);
 
-  // Ultrassônicos
   pinMode(TRIG_1, OUTPUT);
   pinMode(ECHO_1, INPUT);
   pinMode(TRIG_2, OUTPUT);
   pinMode(ECHO_2, INPUT);
 
-  // // Bombas
-  // pinMode(BOMBA1P, OUTPUT);
-  // pinMode(BOMBA1N, OUTPUT);
+  pinMode(BOMBA1P, OUTPUT);
+  pinMode(BOMBA1N, OUTPUT);
   pinMode(BOMBA2P, OUTPUT);
   pinMode(BOMBA2N, OUTPUT);
+  pinMode(backLight, OUTPUT);
 
-  // analogWrite(BOMBA1P, LOW);
-  // analogWrite(BOMBA1N, LOW);
+  analogWrite(BOMBA1P, LOW);
+  analogWrite(BOMBA1N, LOW);
   analogWrite(BOMBA2P, LOW);
   analogWrite(BOMBA2N, LOW);
+
+  lcd.begin(16, 2);
+  lcd.print("SuperMaker");
+
+  digitalWrite(backLight, HIGH);
 }
 
 void loop() {
-
-  // Atualiza comunicação Modbus
   modbus_update();
 
-  // Atualiza distância lida pelos ultrassônicos
   holdingRegs[DIST_1] = readUltrasonic(TRIG_1, ECHO_1);
   holdingRegs[DIST_2] = readUltrasonic(TRIG_2, ECHO_2);
 
-  // Controle da Bomba 2 pelo Modbus (0–100%)
   int bomba2_percent = holdingRegs[BOMBA2_PWM];
   if (bomba2_percent > 100) bomba2_percent = 100;
-
   ligaBomba2(bomba2_percent);
 
-  delay(100);
+  int bomba1_percent = holdingRegs[BOMBA1_PWM];
+  if (bomba1_percent > 100) bomba1_percent = 100;
+  ligaBomba1(bomba1_percent);
+
+  if (millis() - ultimoTempo >= intervalo) {
+
+    static int teclaAnt = -1;
+
+    int leitura = analogRead(A0);
+
+    int teclaNova;
+    for (teclaNova = 0;; teclaNova++) {
+      if (leitura < teclas[teclaNova].limite) {
+        break;
+      }
+    }
+
+    if (teclaNova != teclaAnt) {
+      lcd.setCursor(0, 1);
+      lcd.print(teclas[teclaNova].nome);
+      teclaAnt = teclaNova;
+    }
+
+    ultimoTempo = millis();
+  }
 }
